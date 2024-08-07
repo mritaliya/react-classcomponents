dc4cd593d49e516c0dc2f5f4b1f1dfe0c3af9c75
mritaliya <160356272+mritaliya@users.noreply.github.com>
2024-08-07 20:42:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/Class.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class welcome extends Component{
+    render(){
+        return <>
+        <h1>hellow</h1>
+        <h1>shiva</h1>
+        </>
+    }
+}
+
+export default welcome;
